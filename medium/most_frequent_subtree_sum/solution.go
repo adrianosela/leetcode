618617c854054d0237619aaff417a40658e6ca6c
Adriano Sela Aviles <adrianosela@protonmail.com>
2019-09-03 16:27:53 -0700

    Update solution.go

--- medium/most_frequent_subtree_sum/solution.go
+++ medium/most_frequent_subtree_sum/solution.go
@@ -1,13 +1,11 @@
 package most_frequent_subtree_sum
 
-/**
- * Definition for a binary tree node.
- * type TreeNode struct {
- *     Val int
- *     Left *TreeNode
- *     Right *TreeNode
- * }
- */
+type TreeNode struct {
+	Val int
+	Left *TreeNode
+	Right *TreeNode
+}
+
 func findFrequentTreeSum(root *TreeNode) []int {
 	if root == nil {
 		return []int{}
